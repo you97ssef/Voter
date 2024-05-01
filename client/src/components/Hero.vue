e4ba4170e63fa236513f1ba255c7c8dfdce54ba1
Youssef BAHI <you97ssef@gmail.com>
2024-05-01 22:05:12 +0200

    done app hero

---
+++ client/src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="my-24 gap-9">
+        <div class="flex justify-center">
+            <img src="@/assets/illustrations/votes.svg" class="max-w-64 w-full" />
+        </div>
+        <div class="mt-10 text-center">
+            <h1 class="text-5xl font-bold font-courgette">Voter</h1>
+            <p class="text-xl py-2">
+                Simple and easy to use polling application. Create your own polls and share them with your friends.
+            </p>
+            <a class="btn btn-primary text-xl m-1">
+                <i class="fa-solid fa-check-to-slot"></i> Get Started
+            </a>
+        </div>
+        <div class="text-center my-4 text-md">
+            <i class="fa-solid fa-arrow-down-long"></i> 
+            Scroll down to view polls
+            <i class="fa-solid fa-arrow-down-long"></i>
+        </div>
+    </div>
+</template>
