2fb098736661b71471b88cfa6072126fe3fd30cb
Youssef BAHI <you97ssef@gmail.com>
2024-05-02 17:58:54 +0200

    add verified at to user model

--- api/models/user.go
+++ api/models/user.go
@@ -1,9 +1,12 @@
 package models
 
+import "time"
+
 type User struct {
-	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Id         int       `json:"id"`
+	Name       string    `json:"name"`
+	Username   string    `json:"username"`
+	Email      string    `json:"email"`
+	Password   string    `json:"password"`
+	VerifiedAt *time.Time `json:"verified_at"`
 }
