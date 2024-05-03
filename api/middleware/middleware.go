4d52b63e61e8a42a6ae53e30606e98121d68f7ce
Youssef BAHI <you97ssef@gmail.com>
2024-05-03 18:14:08 +0200

    add connected middleware

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -1,7 +1,27 @@
 package middleware
 
-import "voter/api/core"
+import (
+	"voter/api/controllers"
+	"voter/api/core"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Middleware struct {
 	Server *core.Server
+}
+
+func (m *Middleware) Connected() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		claims, err := m.Server.Jwt.VerifyTokenFromGinContext(c)
+
+		if err != nil {
+			controllers.Unauthorized(c, "Token not provided or invalid")
+			return
+		}
+
+		c.Set("claims", claims)
+
+		c.Next()
+	}
 }
