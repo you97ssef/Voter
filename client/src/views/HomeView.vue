eec3433c359932fda3d1d57697e9ff31cd820f6e
Youssef BAHI <you97ssef@gmail.com>
2024-05-05 13:17:10 +0200

    cleaned app

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    HelloWorld
   </main>
 </template>
