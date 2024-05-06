ab4ce19e91695673cb87373ec99133b81198bd76
Youssef BAHI <you97ssef@gmail.com>
2024-05-06 22:07:45 +0200

    configure app and add views to router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,14 @@
+<script setup>
+    import Loading from './components/Loading.vue'
+    import Toast from './components/Toast.vue'
+    import Header from './components/Header.vue'
+</script>
+
 <template>
-  <RouterView />
+    <Loading />
+    <Toast />
+    <Header />
+    <div class="p-4">
+        <RouterView />
+    </div>
 </template>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,15 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
+import VerifyView from '@/views/auth/VerifyView.vue'
+import { useConnectionStore } from '@/stores/connection'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView,
+            beforeEnter: authGuard
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView,
+            beforeEnter: authGuard
+        },
+        {
+            path: '/verify',
+            name: 'verify',
+            component: VerifyView,
+        }
+    ]
 })
+
+function authGuard(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
+    if (useConnectionStore().isAuthenticated) {
+        next({ name: 'home' })
+    } else next()
+}
+
 
 export default router
