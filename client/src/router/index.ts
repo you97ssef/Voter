d96899439a3050260fe9e0cf65982f0e98dabf29
Youssef BAHI <you97ssef@gmail.com>
2024-05-11 22:20:21 +0200

    configured router and prettier

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,35 +1,36 @@
 import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import VerifyView from '@/views/auth/VerifyView.vue'
+
+import MyPollsView from '@/views/polls/MyPollsView.vue'
+import PollView from '@/views/polls/PollView.vue'
+import NewPollView from '@/views/polls/NewPollView.vue'
+
+import NotFoundView from '@/views/NotFoundView.vue'
+
 import { useConnectionStore } from '@/stores/connection'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: LoginView,
-            beforeEnter: authGuard
-        },
-        {
-            path: '/register',
-            name: 'register',
-            component: RegisterView,
-            beforeEnter: authGuard
-        },
-        {
-            path: '/verify',
-            name: 'verify',
-            component: VerifyView,
-        }
+        { path: '/', name: 'home', component: HomeView },
+
+        { path: '/login', name: 'login', component: LoginView, beforeEnter: authGuard },
+        { path: '/register', name: 'register', component: RegisterView, beforeEnter: authGuard },
+        { path: '/verify', name: 'verify', component: VerifyView },
+
+        { path: '/my-polls', name: 'my-polls', component: MyPollsView },
+        { path: '/polls/:id', name: 'poll', component: PollView },
+        { path: '/poll-by-code/:code', name: 'private-poll', component: PollView },
+        { path: '/new-poll', name: 'new-poll', component: NewPollView },
+
+        { path: '/404', name: 'not-found', component: NotFoundView },
+
+        { path: '/:pathMatch(.*)*', redirect: { name: 'not-found' } }
     ]
 })
 
@@ -38,6 +39,5 @@ function authGuard(to: RouteLocationNormalized, from: RouteLocationNormalized, n
         next({ name: 'home' })
     } else next()
 }
-
 
 export default router
