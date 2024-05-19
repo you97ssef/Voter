5b2c0ce80e9827a2483c4845bf28acc29c48f93e
Youssef BAHI <you97ssef@gmail.com>
2024-05-19 18:22:20 +0200

    add needed fields to vote and add type verifier

--- client/src/models/vote.ts
+++ client/src/models/vote.ts
@@ -2,8 +2,13 @@ export interface Vote {
     id: number;
     user_id: number;
     option_id: number;
-    poll_id: number;
     guest: string | null;
+    poll_id: number;
+    
+    timestamp: number;
+    hash: string;
+    prev_hash: string;
+
     valid: boolean | undefined;
 }
 
@@ -31,8 +36,22 @@ export function buildValidations(votes: Vote[], validations: ValidatedVote[] ): 
 
         if (validation) {
             vote.valid = validation.valid;
+        } else {
+            vote.valid = false;
         }
 
         return vote;
     });
+}
+
+export function isVote(vote: any): boolean {
+    return vote.id !== undefined
+        && vote.user_id !== undefined
+        && vote.option_id !== undefined
+        && vote.poll_id !== undefined
+        && vote.guest !== undefined
+        && vote.valid !== undefined
+        && vote.timestamp !== undefined
+        && vote.hash !== undefined
+        && vote.prev_hash !== undefined;
 }
