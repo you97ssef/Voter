aeedb69ee9793413b4ee11209a06a5e79f7b93d7
Youssef BAHI <you97ssef@gmail.com>
2024-05-29 23:44:54 +0200

    refactor mailer to be up to standards

--- api/core/services/mailer.go
+++ api/core/services/mailer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/smtp"
 	"strings"
+	"time"
 )
 
 func NewMailer(username, password, host, port string) *Mailer {
@@ -33,11 +34,21 @@ func (m *Mailer) SendEmail(to []string, subject, body string) error {
 }
 
 func (m *Mailer) buildMessage(to []string, subject, body string) []byte {
-	msg := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\r\n"
-	msg += fmt.Sprintf("From: %s\r\n", m.username)
+	msg := fmt.Sprintf("From: %s\r\n", m.username)
 	msg += fmt.Sprintf("To: %s\r\n", strings.Join(to, ";"))
+	msg += fmt.Sprintf("Reply-To: %s\r\n", m.username)
+
 	msg += fmt.Sprintf("Subject: %s\r\n", subject)
-	msg += fmt.Sprintf("\r\n%s\r\n", body)
+	msg += fmt.Sprintf("Date: %s\r\n", time.Now().Format(time.RFC1123Z))
+
+	msg += "MIME-Version: 1.0\r\n"
+	
+	msg += "Content-Type: text/html; charset=\"UTF-8\"\r\n"
+	msg += "Content-Transfer-Encoding: 7bit\r\n"
+
+	msg += "\r\n"
+
+	msg += fmt.Sprintf("<!DOCTYPE html><html><body>%s</body></html>\r\n", body)
 
 	return []byte(msg)
 }
